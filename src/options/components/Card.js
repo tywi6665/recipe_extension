f2e80fda8ff83a54be30ab28f745da90ebc43673
tywi6665 <tyletcole@gmail.com>
2020-09-20 20:50:23 -0600

    created Card component shell

---
+++ src/options/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({ title }) => {
+    return (
+        <div className="card">
+            <h3>{Title}</h3>
+        </div>
+    );
+}
+
+export default Card;
